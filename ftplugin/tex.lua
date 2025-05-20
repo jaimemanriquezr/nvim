eaa192b5e6f7d11c428b2301910fd4b5f56d11d0
JaimeManriquezR <jr.manriquezr@gmail.com>
2025-05-20 13:48:22 +0200

    added tex config

---
+++ ftplugin/tex.lua
@@ -0,0 +1,4 @@
+vim.g.tex_conceal = 'abdmgs'
+vim.opt_local.conceallevel = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
--- init.lua
+++ init.lua
@@ -159,9 +159,10 @@ require('lazy').setup(
       init = function()
         -- VimTeX configuration goes here, e.g.
         vim.g.vimtex_view_method = 'zathura'
-        vim.g_vimtex_view_general_viewer = 'okular'
+        vim.g.vimtex_view_general_viewer = 'okular'
         vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
         vim.g.vimtex_compiler_method = 'latexmk'
+
         -- vim.g.vimtex_syntax_conceal = true
       end,
     },
