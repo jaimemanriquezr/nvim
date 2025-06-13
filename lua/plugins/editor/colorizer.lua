283af6d668801cf2502be4389a54ce85a3c78c6e
JaimeManriquezR <jr.manriquezr@gmail.com>
2025-06-13 18:26:21 +0200

    added nvim colorizer

---
+++ lua/plugins/editor/colorizer.lua
@@ -0,0 +1,9 @@
+-- colorizer
+-- https://github.com/catgoose/nvim-colorizer.lua
+-- The fastest Neovim colorizer
+return {
+  'catgoose/nvim-colorizer.lua',
+  event = 'BufReadPre',
+  opts = { -- set to setup table
+  },
+}
