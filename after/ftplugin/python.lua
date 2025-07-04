4889f4e60c5be013b214172f364dfd582b68a2fd
solgatan <jr.manriquezr@gmail.com>
2025-07-04 11:17:16 +0200

    changed cc in python to 72

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,6 +1,6 @@
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.o.colorcolumn = '92'
+vim.o.colorcolumn = '72'
 vim.o.expandtab = true
 
 vim.keymap.set('n', '<F9>', ':w | !clear; python3 %<CR>', { desc = 'Run Python script' })
