2e2ad7815ab8d78d3dea73763d0b9893a3d3e55e
JaimeManriquezR <jr.manriquezr@gmail.com>
2025-08-29 18:07:42 +0200

    fixed deprecated function and increased timeout time

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,113 +1,119 @@
 vim.lsp.config('*', {
-  capabilities = {
-    textDocument = {
-      semanticTokens = {
-        multilineTokenSupport = true,
-      },
-    },
-  },
-  root_markers = { '.git' },
+	capabilities = {
+		textDocument = {
+			semanticTokens = {
+				multilineTokenSupport = true,
+			},
+		},
+	},
+	root_markers = { '.git' },
 })
 
 -- https://github.com/onosendi/dotfiles/blob/master/common/.config/nvim/init.lua
 -- Then load all other LSP configs
 local lsp_path = vim.fn.stdpath 'config' .. '/lua/lsp'
 for _, file in ipairs(vim.fn.readdir(lsp_path)) do
-  if file:match '%.lua$' and file ~= 'global.lua' then
-    local ls_name = file:gsub('%.lua$', '')
-    vim.lsp.enable(ls_name)
-    -- extend basic config (from nvim-lspconfig) with local config file
-    vim.lsp.config(ls_name, require('lsp.' .. ls_name))
-  end
+	if file:match '%.lua$' and file ~= 'global.lua' then
+		local ls_name = file:gsub('%.lua$', '')
+		vim.lsp.enable(ls_name)
+		-- extend basic config (from nvim-lspconfig) with local config file
+		-- vim.lsp.config(ls_name, require('lsp.' .. ls_name))
+	end
 end
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my.lsp', {}),
-  callback = function(args)
-    local map = function(keys, func, desc, mode)
-      mode = mode or 'n'
-      vim.keymap.set(mode, keys, func, { buffer = args.buf, desc = 'LSP: ' .. desc })
-    end
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		local map = function(keys, func, desc, mode)
+			mode = mode or 'n'
+			vim.keymap.set(mode, keys, func, { buffer = args.buf, desc = 'LSP: ' .. desc })
+		end
 
-    -- Jump to the definition of the word under your cursor.
-    --  To jump back, press <C-t>.
-    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+		-- Jump to the definition of the word under your cursor.
+		--  To jump back, press <C-t>.
+		map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
-    -- Find references for the word under your cursor.
-    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+		-- Find references for the word under your cursor.
+		map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
-    -- Jump to the implementation of the word under your cursor.
-    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+		-- Jump to the implementation of the word under your cursor.
+		map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
-    -- Jump to the type of the word under your cursor.
-    map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+		-- Jump to the type of the word under your cursor.
+		map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
 
-    -- Fuzzy find all the symbols in your current document.
-    map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+		-- Fuzzy find all the symbols in your current document.
+		map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
 
-    -- Fuzzy find all the symbols in your current workspace.
-    map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+		-- Fuzzy find all the symbols in your current workspace.
+		map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-    -- Rename the variable under your cursor.
-    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+		-- Rename the variable under your cursor.
+		map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
-    -- Execute a code action, usually your cursor needs to be on top of an error
-    -- or a suggestion from your LSP for this to activate.
-    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+		-- Execute a code action, usually your cursor needs to be on top of an error
+		-- or a suggestion from your LSP for this to activate.
+		map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
-    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+		map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-      local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-        buffer = args.buf,
-        group = highlight_augroup,
-        callback = vim.lsp.buf.document_highlight,
-      })
+		map('gf', function()
+			vim.lsp.buf.format {
+				timeout_ms = 20000,
+			}
+		end, 'Format buffer')
 
-      if not vim.g.vscode then
-        vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-          buffer = args.buf,
-          group = highlight_augroup,
-          callback = vim.lsp.buf.clear_references,
-        })
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method 'textDocument/documentHighlight' then
+			local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight',
+				{ clear = false })
+			vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+				buffer = args.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.document_highlight,
+			})
 
-        vim.api.nvim_create_autocmd('LspDetach', {
-          group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-          callback = function(event2)
-            vim.lsp.buf.clear_references()
-            vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-          end,
-        })
-      end
-    end
+			if not vim.g.vscode then
+				vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+					buffer = args.buf,
+					group = highlight_augroup,
+					callback = vim.lsp.buf.clear_references,
+				})
 
-    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-      map('<leader>th', function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = args.buf })
-      end, '[T]oggle Inlay [H]ints')
-    end
-    if client:supports_method 'textDocument/implementation' then
-      -- Create a keymap for vim.lsp.buf.implementation ...
-    end
-    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-    if client:supports_method 'textDocument/completion' then
-      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-      -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-      -- client.server_capabilities.completionProvider.triggerCharacters = chars
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
-    end
-    -- Auto-format ("lint") on save.
-    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-    if not client:supports_method 'textDocument/willSaveWaitUntil' and client:supports_method 'textDocument/formatting' then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format { bufnr = args.buf, id = client.id, timeout_ms = 1000 }
-        end,
-      })
-    end
-  end,
+				vim.api.nvim_create_autocmd('LspDetach', {
+					group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+					callback = function(event2)
+						vim.lsp.buf.clear_references()
+						vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+					end,
+				})
+			end
+		end
+
+		if client:supports_method 'textDocument/inlay_hint' then
+			map('<leader>th', function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = args.buf })
+			end, '[T]oggle Inlay [H]ints')
+		end
+
+		-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+		if client:supports_method 'textDocument/completion' then
+			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
+			-- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			-- client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+		end
+
+		-- Auto-format ("lint") on save.
+		-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+		if not client:supports_method 'textDocument/willSaveWaitUntil' and client:supports_method 'textDocument/formatting' then
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.format { bufnr = args.buf, id = client.id, timeout_ms = 20000 }
+				end,
+			})
+		end
+	end,
 })
