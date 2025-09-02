429f44472d1114565034c24dfcf768323617772d
JaimeManriquezR <jr.manriquezr@gmail.com>
2025-09-02 17:50:30 +0200

    added version to luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -3,22 +3,23 @@
 -- Snippet Engine for Neovim written in Lua
 
 return {
-  'L3MON4D3/LuaSnip',
-  build = (function()
-    if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-      return
-    end
-    return 'make install_jsregexp'
-  end)(),
-  dependencies = {
-    -- `friendly-snippets` contains a variety of premade snippets.
-    --    See the README about individual language/framework/plugin snippets:
-    --    https://github.com/rafamadriz/friendly-snippets
-    {
-      'rafamadriz/friendly-snippets',
-      config = function()
-        require('luasnip.loaders.from_vscode').lazy_load()
-      end,
-    },
-  },
+	'L3MON4D3/LuaSnip',
+	version = 'v2.*',
+	build = (function()
+		if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+			return
+		end
+		return 'make install_jsregexp'
+	end)(),
+	dependencies = {
+		-- `friendly-snippets` contains a variety of premade snippets.
+		--    See the README about individual language/framework/plugin snippets:
+		--    https://github.com/rafamadriz/friendly-snippets
+		{
+			'rafamadriz/friendly-snippets',
+			config = function()
+				require('luasnip.loaders.from_vscode').lazy_load()
+			end,
+		},
+	},
 }
